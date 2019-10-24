fdad0710a8ba9d21f5806be7a803c6784f621cf2
mohaimen.khan@mail.utoronto.ca <mohaimen.khan@mail.utoronto.ca>
2019-10-24 19:16:06 -0400

    added heroku setup

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,35 @@
+require('dotenv').config()
 var createError = require('http-errors');
+var cors = require('cors');
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
+// API routers
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+// react route setup
+app.use(express.static(path.join(__dirname, "../frontend/public")));
+// react root
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "../frontend/public/index.html"));
+})
+// MongoDB SetUp:
+
+let dev_url = 'mongodb://localhost:27017/shopping'
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,6 +37,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
